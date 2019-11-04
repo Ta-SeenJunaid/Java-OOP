19e000783ef2a3df7b417854fa2aa9b032bb6b97
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-11-04 20:24:12 +0600

    Teacher class

---
+++ Application/src/inheritance/Teacher.java
@@ -0,0 +1,16 @@
+
+package inheritance;
+
+
+public class Teacher extends Person {
+    String qualification;
+    
+    void display2(){
+        //System.out.println("Name: "+ name);
+        //System.out.println("Age: "+ age);
+        display1();
+        System.out.println("Qualification: "+ qualification);
+        
+    }
+    
+}
