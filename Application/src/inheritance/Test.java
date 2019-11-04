f6820dea6ba4689a7c9bf0048e084f3dd7e85ddd
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-11-04 20:24:38 +0600

    Inheritance

---
+++ Application/src/inheritance/Test.java
@@ -0,0 +1,18 @@
+
+package inheritance;
+
+
+public class Test {
+    
+    public static void main(String []args){
+        
+        Teacher t1 = new Teacher();
+        
+        t1.name = "dkfl dfds";
+        t1.age = 54;
+        t1.qualification = "dflkdsjl sdjfkd";
+        
+        t1.display2();
+    }
+    
+}
