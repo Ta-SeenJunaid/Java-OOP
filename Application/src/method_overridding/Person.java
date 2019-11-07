a16b5748493fb4b7c85a4df9ebca8d72b2973e5a
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-11-07 19:11:49 +0600

    Person Class

---
+++ Application/src/method_overridding/Person.java
@@ -0,0 +1,14 @@
+
+package method_overridding;
+
+
+public class Person {
+    String name;
+    int age;
+    
+    void display(){
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+    }
+    
+}
