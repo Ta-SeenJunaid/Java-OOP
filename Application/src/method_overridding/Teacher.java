5dc0d9efcf524c2f0cbd4dc2d5eedc3d51abc0c3
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-11-07 19:12:48 +0600

    Teacher Class

---
+++ Application/src/method_overridding/Teacher.java
@@ -0,0 +1,16 @@
+
+package method_overridding;
+
+
+public class Teacher extends Person {
+    
+    String qualification;
+
+    @Override
+    void display(){
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Qualification: "+qualification);
+    }
+    
+}
